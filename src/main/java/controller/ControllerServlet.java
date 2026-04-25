8ea3ad7ddb35d8ecbf6c2b3dc01b90f54891ed15
Lucas Alves Rosa <lucasar29000@gmail.com>
2026-04-25 15:00:37 +0200

    oubli d'url pattern dans ControllerServlet

--- src/main/java/controller/ControllerServlet.java
+++ src/main/java/controller/ControllerServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Servlet implementation class ControllerServlet
  */
 
-@WebServlet(urlPatterns = {"/index", "/accueil", "/connexion"})
+@WebServlet(urlPatterns = {"/index", "/accueil", "/connexion", "/inscription"})
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
