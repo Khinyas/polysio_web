7eb3b153f4bc364d8ed0402d896cc291564e8a9c
Jardon Jean-Luc <kenoh91@gmail.com>
2026-04-25 12:22:13 +0200

    wip2

---
+++ src/main/webapp/JS/contenu-template.js
@@ -0,0 +1,24 @@
+const PLATEAU = [
+                {
+                    algo-id:
+                    algo-nom
+                    algo-positionY
+                    algo-typeCase
+                    algo-positionId
+                    algo-positionX
+                    imageSrc:  '/images/projets/xxx.png',
+                    title:     'Projet Monopoly',
+                    description:      'Application de jeu Monopoly développée en Java (Swing).',
+                    id: 'monopoly', // <--- IMPORTANT : l'identifiant,
+                    btnText:   'Voir Projet →',
+                    btnClass:  'btn-card-secondary'
+                }
+                ];
+
+                	private int id;
+                	private String nom;
+                	private int positionY;
+                	private String typeCase;
+                	private int positionId;
+                	private int positionX;
+                	private String cheminSvg;
