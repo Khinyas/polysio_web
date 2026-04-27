574fc2a35597421ae1421373d5733aa5c3e182b2
Jean-Luc Jardon <kenoh91@gmail.com>
2026-04-27 11:43:11 +0200

    rajout resources as sources

---
+++ src/main/webapp/polysio_web/JS/contenu-template.js
@@ -0,0 +1,51 @@
+const PLATEAU = [
+                {
+                    imageSrc:  '/asset/Plateau/Case/0.png',
+                    title:     'case + `X`',
+                    description:      'Case du plateau',
+                    id: 'plateau',
+                    btnText:   'Voir Propriete',
+                    btnClass:  'btn-casePlateau'
+                },
+                {
+                    imageSrc:  '/asset/Plateau/Case/1.png',
+                    title:     'case + `X`',
+                    description:      'Case du plateau',
+                    id: 'plateau',
+                    btnText:   'Voir Propriete',
+                    btnClass:  'btn-casePlateau'
+                }
+                ];
+
+
+/**
+ * Injection dynamique dans le DOM
+ */
+function injecterContenuIndex(contenu) {
+    const zonePlateau = document.getElementById('plateau');
+    if (!zonePlateau) return;
+
+    let html = PLATEAU.map(p => genererCardHTML(p)).join('');
+
+    zonePlateau.innerHTML = html;
+
+}
+
+function genererCardHTML(plateauP) {
+    return `
+        <div class="case-app">
+            <div class="case-image-box">
+                <div class="case-image-fallback" style="background: linear-gradient(135deg,#1c2330,#2a3444);">
+                    <img src="${plateauP.imageSrc}" alt="${plateauP.title}" loading="lazy">
+                </div>
+            </div>
+            <div class="case-body">
+                <h3>${plateauP.title}</h3>
+                <p>${plateauP.description}</p> <button class="btn-case ${plateauP.btnClass}">${plateauP.btnText} →</button> </div>
+        </div>
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    injecterContenuIndex();
+});
---
+++ src/main/webapp/polysio_web/WEB-INF/template.jsp
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/CSS/css_style.css">
+    <script type="module" src="${pageContext.request.contextPath}/JS/contenu-template.js"></script>
+</head>
+<body>
+    <header>
+        <nav id="header">Logo PolySIO | Accueil | Profil</nav>
+    </header>
+
+    <section class="hero">
+        <h1>Bienvenue sur PolySIO</h1>
+        <div id="plateau">
+        <!-- Contenu du Plateau -->
+            </div>
+    </section>
+
+    <main>
+<!-- Contenu Annexe : Statistiques, Message etc -->
+    </main>
+
+    <footer id="footer">
+        <p>Projet de PPE - Groupe Rouge - BTS SIO </p>
+        <p>Réalisé par Lucas Alves Rosa, Anthony Simon, Jardon Jean-Luc, Yoan Say</p>
+    </footer>
+</body>
+</html>
