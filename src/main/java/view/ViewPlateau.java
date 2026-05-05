ad7f3ffd93c57e6ca4d1f41c5817914315c236ee
Jean-Luc Jardon <kenoh91@gmail.com>
2026-05-05 16:28:44 +0200

    affichage plateau

--- src/main/java/view/ViewPlateau.java
+++ src/main/java/view/ViewPlateau.java
@@ -38,14 +38,18 @@ public class ViewPlateau extends HttpServlet {
         List<ModelCase> listeCases = (List<ModelCase>) request.getAttribute("listeCasesBdd");
         StringBuilder cases = new StringBuilder();
         for (ModelCase casePlateau : listeCases) {
+            // On convertit les positions 0-10 en positions CSS 1-11
+            int cssX = casePlateau.getPositionX() + 1;
+            int cssY = casePlateau.getPositionY() + 1;
+
             if (casePlateau.getNom().equals("centre")) {
                 cases.append(String.format(
-                    "<div id='%s' class='case' style='grid-column:%d / span 2; grid-row:%d / span 2; '>" +
+                    "<div id='%s' class='case' style='grid-column:%d / span 9; grid-row:%d / span 9; '>" +
                     "<img src='%s' alt='%s' width='100%%' height='100%%' />" + 
                     "</div>",
                     casePlateau.getIdCSS(),
-                    casePlateau.getPositionX(),
-                    casePlateau.getPositionY(),
+                    2, // Le centre commence à la colonne 2 (après la 1ère case)
+                    2, // Le centre commence à la ligne 2
                     request.getContextPath() + casePlateau.getCheminSvg(),
                     casePlateau.getNom()
                 ));
@@ -55,8 +59,8 @@ public class ViewPlateau extends HttpServlet {
                     "<img src='%s' alt='%s' width='100%%' height='100%%' />" + 
                     "</div>",
                     casePlateau.getIdCSS(),
-                    casePlateau.getPositionX(),
-                    casePlateau.getPositionY(),
+                    cssX, // Utilise la position corrigée (+1)
+                    cssY, // Utilise la position corrigée (+1)
                     request.getContextPath() + casePlateau.getCheminSvg(),
                     casePlateau.getNom()
                 ));
@@ -64,18 +68,44 @@ public class ViewPlateau extends HttpServlet {
         }
         
         String html = """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>Mon Plateau</title>
-                </head>
-                <body>
-                    <div class="plateau">
-                    	%s
-                    </div>
-                </body>
-            </html>
-            """.formatted(cases.toString());
+        	    <!DOCTYPE html>
+        	    <html>
+        	        <head>
+        	            <title>Mon Plateau</title>
+        	            <link rel="stylesheet" type="text/css" href="%s/css/style.css">
+        	            <style>
+        	                .plateau {
+        	                    display: grid;
+        	                    grid-template-columns: repeat(11, 1fr);
+        	                    grid-template-rows: repeat(11, 1fr);
+        	                    width: 95vmin; 
+        	                    height: 95vmin;
+        	                    margin: auto;
+        	                    grid-gap: 0; 
+        	                    border: 2px solid black;
+        	                }
+        	                .case {
+        	                    border: 1px solid black;
+        	                    padding: 0;
+        	                    margin: 0;
+        	                    display: flex;
+        	                    overflow: hidden;
+        	                }
+        	                .case img {
+        	                    width: 100%%; /* DOUBLE POURCENTAGE ICI */
+        	                    height: 100%%; /* DOUBLE POURCENTAGE ICI */
+        	                    object-fit: fill; 
+        	                    display: block;
+        	                }
+        	            </style>
+        	        </head>
+        	        <body>
+        	            <div class="plateau">
+        	                %s
+        	            </div>
+        	        </body>
+        	    </html>
+        	    """.formatted(request.getContextPath(), cases.toString());
 
         out.print(html);
         // On vide le buffer vers le navigateur sans fermer brutalement
