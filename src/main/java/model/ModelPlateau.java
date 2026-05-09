8ae08280346e09b1a385066c2c981cc8fc49b845
Jardon Jean-Luc <kenoh91@gmail.com>
2026-05-09 12:07:21 +0200

    rajout controller propriete rajouté et wip sur le controllerconstructeurplateau

--- src/main/java/model/ModelPlateau.java
+++ src/main/java/model/ModelPlateau.java
@@ -6,34 +6,14 @@ import java.util.List;
 // On utilise StackPane pour pouvoir empiler le background ET le plateau par dessus
 public class ModelPlateau  {
     private List<ModelCase> listeCases;
-    //private List<ViewCase> listeViewCases = new ArrayList<>();
-    private List<ModelPropriete> listePropriete;
-    //private List<ViewPropriete> listeViewPropriete = new ArrayList<>();;
+    private List<ModelPropriete> listeProprietes;
     private ArrayList<ModelJoueur> listeJoueurs;
 
     public ModelPlateau(List<ModelCase> listeCasesP, List<ModelPropriete> listeProprieteP) {
         this.listeCases = listeCasesP;
-        this.listePropriete = listeProprieteP;
-        /*
-        this.prefHeightProperty().bind(this.heightProperty().multiply(0.8));
-        this.prefWidthProperty().bind(this.heightProperty().multiply(0.8));
-         */
+        this.listeProprietes = listeProprieteP;
+        listeJoueurs = new ArrayList<>();
 
-       // this.setAlignment(Pos.CENTER);
-        //this.setGridLinesVisible(false); // Mets à true pour débugger et voir la grille !
-
-        // On force une taille minimale pour le test si le bind échoue
-        //this.setMinSize(400, 400);
-        //this.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
-
-        // 0, 0, 0 = Noir | 1.0 = Opacité
-        //this.setStyle("-fx-background-color: rgba(0, 0, 0, 1.0);");
-
-        // Espacement entre cellule grille (optionnel) :
-        /*
-        this.plateau.setHgap(2);
-        this.plateau.setVgap(2);
-        */
         //this.initialiserPlateau();
     }
 
@@ -42,38 +22,16 @@ public class ModelPlateau  {
     }
 
     public List<ModelPropriete> getListePropriete() {
-        return listePropriete;
+        return listeProprietes;
     }
 
-   /* public void initialiserPlateau() {
-        // On clear toujours la gridPane pour s'assurer qu'elle est vide
-        this.getChildren().clear();
-        this.getColumnConstraints().clear();
-        this.getRowConstraints().clear();
-
-        // Je crée une boucle pour assigner des positions à chaque case de la liste
-        // Important, les cases doivent être Ordonnées dans la liste
-        for (int i = 0; i < 11; i++) {
-            ColumnConstraints col = new ColumnConstraints();
-            RowConstraints row = new RowConstraints();
-            if (i == 0 || i == 10) {
-                col.setPercentWidth(12.5);
-                row.setPercentHeight(12.5);
-            } else {
-                col.setPercentWidth(8.3);
-                row.setPercentHeight(8.3);
-            }
-            this.getColumnConstraints().add(col);
-            this.getRowConstraints().add(row);
-        }
-
+   public void initialiserPlateau() {
         // Inserer controller plateau pour créer le plateau graphique à partir des model cases
         for (ModelCase caseModelCase : this.listeCases) {
             // On ignore la case centrale ici pour ne pas l'ajouter en 1x1 dans la boucle
             if (caseModelCase.getId() == 99 || caseModelCase.getNom().equalsIgnoreCase("centre")) {
                 continue;
             }
-
             ViewCase casePlateau = new ViewCase(caseModelCase);
 
 // FORCE LA CASE À PRENDRE TOUTE LA PLACE DE LA CELLULE
@@ -97,21 +55,9 @@ public class ModelPlateau  {
 
         // Constuire les Cartes de proprietees :
 
-        for (ModelPropriete propriete : this.listePropriete) {
+        for (ModelPropriete propriete : this.listeProprietes) {
             ViewPropriete viewPropriete = new ViewPropriete(propriete);
             listeViewPropriete.add(viewPropriete);
         }
     }
-
-    public List<ViewCase> getListeViewCases() {
-        return listeViewCases;
-    }
-    /*public ViewCase getViewCase(int idP) {
-        return listeViewCases.get(idP);
-    }*/
-
-
-    
-    
-
 }
